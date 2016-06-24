e9bcaf328b98ae8eee94e75f34f1a857a8bec6ec
christian-bromann <contact@christian-bromann.com>
2016-06-23 23:33:10 -0700

    disable wdio logs

--- test/helper.js
+++ test/helper.js
@@ -25,11 +25,47 @@ export function clean () {
 }
 
 export function run (specs) {
+    disableOutput()
+
     const launcher = new Launcher('./test/fixtures/wdio.conf.js', {
         specs: specs.map(spec => `./test/fixtures/specs/${spec}.js`)
     })
 
     return launcher.run().then(() => {
+        enableOutput()
         return getResults()
     })
+}
+
+let logs, originalConsole
+
+function disableOutput () {
+    if (process.env.FULL_OUTPUT) {
+        return
+    }
+    const mockLog = (type) => (...message) => {
+        logs[type].push(message.join(' '))
+    }
+    logs = {
+        log: [],
+        warn: [],
+        error: []
+    }
+    originalConsole = {
+        log: console.log,
+        warn: console.warn,
+        error: console.error
+    }
+    console.log = mockLog('log')
+    console.warn = mockLog('warn')
+    console.error = mockLog('error')
+}
+
+function enableOutput () {
+    if (process.env.FULL_OUTPUT) {
+        return
+    }
+    console.log = originalConsole.log
+    console.warn = originalConsole.warn
+    console.error = originalConsole.error
 }
