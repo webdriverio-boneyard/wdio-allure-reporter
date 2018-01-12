c36f50ab85a9fb363b6383899c72333c009f5969
netmajor <Netmajor@MacBook-Pro-Lukasz.local>
2018-01-12 17:57:25 +0100

    Fix tests for features

--- test/specs/feature.js
+++ test/specs/feature.js
@@ -12,9 +12,8 @@ describe('Features', () => {
             expect(result('ns2\\:test-suite > name').text()).to.be.equal('Suite with features')
             expect(result('test-case > name').eq(0).text()).to.be.equal('First case')
             expect(result('test-case > name').eq(1).text()).to.be.equal('Second case')
-            expect(result('test-case').attr('status')).to.be.equal('passed')
-            expect(result('test-case label[value="Test feature 1"]').to.have.lengthOf
-            expect(result('test-case label[value="Test feature 2"]')).eq(1).to.be.equal('Test feature 2')
+            expect(result('test-case label[name="feature"]').eq(0).attr('value')).to.be.equal('Test feature 1')
+            expect(result('test-case label[name="feature"]').eq(1).attr('value')).to.be.equal('Test feature 2')
         })
     })
 })
