2f8162d0f6ac6024dec739368c271cea3543b4c6
Lukasz Bartczak <netmajor@gmail.com>
2018-01-24 21:03:51 +0100

    Analytics data

--- test/specs/test-case.js
+++ test/specs/test-case.js
@@ -54,4 +54,18 @@ describe('test cases', () => {
             expect(result('test-case').eq(1).attr('start')).to.be.equal(result('test-case').eq(1).attr('stop'))
         })
     })
+
+    it('should detect analytics labels in test case', () => {
+        return run(['passing']).then((results) => {
+            expect(results).to.have.lengthOf(1)
+            const result = results[0]
+
+            expect(result('ns2\\:test-suite > name').text()).to.be.equal('A passing Suite')
+            expect(result('test-case > name').text()).to.be.equal('with passing test')
+            expect(result('test-case').attr('status')).to.be.equal('passed')
+            expect(result('test-case parameter[kind="environment-variable"]')).to.have.lengthOf(2)
+            expect(result('test-case label[name="language"]').eq(0).attr('value')).to.be.equal('javascript')
+            expect(result('test-case label[name="framework"]').eq(0).attr('value')).to.be.equal('wdio')
+        })
+    })
 })
