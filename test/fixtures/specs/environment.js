4f4e7e19e373e9ce5dd147183b301f7c0b5694ff
Boris Osipov <osipov.boris@gmail.com>
2018-03-15 20:48:13 +0300

    Fix tests
    Signed-off-by: Boris Osipov <osipov.boris@gmail.com>

--- test/fixtures/specs/environment.js
+++ test/fixtures/specs/environment.js
@@ -4,11 +4,10 @@ const reporter = require('./../../../build/reporter')
 
 describe('Suite with environments', () => {
     it('First case', () => {
-        reporter.addEnvironment('BROWSER', 'Chrome')
         reporter.addEnvironment('ENVIRONMENT', 'TEST')
     })
 
     it('Second case', () => {
-        reporter.addEnvironment('BROWSER', 'Firefox')
+        reporter.addEnvironment('ENVIRONMENT', 'Firefox')
     })
 })
